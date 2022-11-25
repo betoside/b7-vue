8af68660fa24aa78c4648bb32071157950525c77
Betoside <betoside@gmail.com>
2022-11-25 18:30:23 -0300

    inicio do treinamento de Vue

---
+++ 03_condicionais_loop.js
@@ -0,0 +1,7 @@
+let placar = new Vue({
+    el:'#placar',
+    data:{
+        n:0,
+        exibir:true
+    }
+});
---
+++ 03_loop.js
@@ -0,0 +1,18 @@
+let teste = new Vue({
+    el:'#teste',
+    data:{
+
+    }
+})
+
+let lista = new Vue({
+    el:"#lista",
+    data:{
+        nomes:[
+            {nome:'jack', sobrenome:'Cast', idade:'99'},
+            {nome:'flavio', sobrenome:'Team', idade:'98'},
+            {nome:'moana', sobrenome:'Havai', idade:'97'},
+            {nome:'kaialani', sobrenome:'Sister', idade:'96'}
+        ]
+    }
+});
