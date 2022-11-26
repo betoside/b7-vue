ad3d92e7430b9f2d7db4aee9c588ec1b527c7d1c
Betoside <betoside@gmail.com>
2022-11-25 23:58:05 -0300

    #11: Sintaxe de template: Interpolações

--- 11_fe_vue_html_basico/script.js
+++ 11_fe_vue_html_basico/script.js
@@ -1,6 +1,11 @@
 let app = new Vue({
 	el:'#app',
 	data:{
+		titulo: 'Título da página',
+		copyright: 'Todos os direitos reservados',
+		subtitulo: '<strong>Este é nosso</strong> subtítulo',
+		nome: 'Apnea Band',
+		carregado: true
 		
 	}
 });
